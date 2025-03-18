2a097076f443fdd08a4ab00eb1af0ab350eb4d88
Vladyslav Stepanenko <vlad.step.186@gmail.com>
2025-03-18 11:19:35 +0200

    Fixed errors

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,18 +6,31 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const cutString = sourceString.split(';');
-  const deleteSpace = cutString.filter((key) => key.trim());
+  // const cutString = sourceString.split(';');
+  // const deleteSpace = cutString.filter((key) => key.trim());
 
-  const result = deleteSpace.reduce((acc, style) => {
-    const [property, value] = style.split(':').map((part) => part.trim());
+  // const result = deleteSpace.reduce((acc, style) => {
+  //   const [property, value] = style.split(':').map((part) => part.trim());
 
-    acc[property] = value;
+  //   acc[property] = value;
 
-    return acc;
-  }, {});
+  //   return acc;
+  // }, {});
 
-  return result;
+  // return result;
+
+
+  const cssObject = sourceString
+    .split(';')
+    .filter(deleteSpace => deleteSpace.trim())
+    .map(style => style.split(':').map(part => part.trim()))
+    .reduce((styles, [property, value]) => {
+      styles[property] = value;
+
+      return styles;
+    }, {});
+
+  return cssObject;
 }
 
 module.exports = convertToObject;
